0ab9338b94edba0250b07f8c7015c6e5d5a04551
monocode777 <147933455+monocode777@users.noreply.github.com>
2025-09-20 00:59:51 +0000

    hash

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from config import Config
 from extensions import db
 from controllers.videojuegos_controller import videojuegos_bp
 
+from flask import Flask
+from controllers.band_controller import band_bp
+from controllers.users_controller import user_bp
+
+app = Flask(__name__)
+
+# Registrar el blueprint de bandas
+app.register_blueprint(band_bp)
+app.register_blueprint(user_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 # Creamos la aplicación Flask
 app = Flask(__name__)
 
